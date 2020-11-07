c2cacb35bbd877deb92dedfce94aee73073d0e89
Marc Oliva <ing.oliva@hotmail.com>
2020-11-07 02:55:01 -0500

    add documentation swagger

--- src/main/java/pe/edu/upc/profile/ProfileServiceApplication.java
+++ src/main/java/pe/edu/upc/profile/ProfileServiceApplication.java
@@ -2,7 +2,6 @@ package pe.edu.upc.profile;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class ProfileServiceApplication {
 
